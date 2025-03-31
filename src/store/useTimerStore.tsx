5485292ae0b02fc3c465a2c47d4ecc23eec6b8f8
Matt Chastain <work@mattchastain.dev>
2025-03-31 16:41:50 -0400

    create setStudyDuration and setBreakDuration functions

--- src/store/useTimerStore.tsx
+++ src/store/useTimerStore.tsx
@@ -4,6 +4,8 @@ import { persist } from 'zustand/middleware';
 type TimerState = {
 	studyDuration: number;
 	breakDuration: number;
+	setStudyDuration: (value: number) => void;
+	setBreakDuration: (value: number) => void;
 };
 
 export const useTimerStore = create<TimerState>()(
@@ -11,6 +13,8 @@ export const useTimerStore = create<TimerState>()(
 		(set) => ({
 			studyDuration: 60 * 45,
 			breakDuration: 60 * 10,
+			setStudyDuration: (value) => set({ studyDuration: value }),
+			setBreakDuration: (value) => set({ breakDuration: value }),
 		}),
 		{ name: 'timer-store' }
 	)
