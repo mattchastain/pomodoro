91b27d5f9da3e0b02dd8496d6d8cb13f5e50100c
Matt Chastain <work@mattchastain.dev>
2025-03-31 15:22:39 -0400

    import Input, add modal close on click

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,4 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { useCallback, useRef } from 'react';
+import { Input } from '..';
 
 interface SettingsProps {
 	settingsOpen: boolean;
@@ -6,16 +8,36 @@ interface SettingsProps {
 }
 
 export function Settings({ settingsOpen, setSettingsOpen }: SettingsProps) {
+	const settingsModalRef = useRef(null);
+
 	return (
 		<AnimatePresence>
 			{settingsOpen && (
-				<motion.div
-					initial={{ scale: 0.9, opacity: 0 }}
-					animate={{ scale: 1, opacity: 1 }}
-					exit={{ scale: 0.9, opacity: 0 }}
-					onClick={() => setSettingsOpen(false)}
-					className='fixed inset-0 backdrop-blur z-40'
-				></motion.div>
+				<>
+					<motion.div
+						ref={settingsModalRef}
+						initial={{ scale: 0.95, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						exit={{ scale: 0.95, opacity: 0 }}
+						onMouseDown={(e) => {
+							if (settingsModalRef.current === e.target) {
+								setSettingsOpen(false);
+							}
+						}}
+						className='fixed inset-0 flex items-center justify-center backdrop-blur z-30'
+					>
+						<div className='flex flex-col gap-4 font-bold md:font-medium'>
+							<div className=''>
+								<h2>Study Duration</h2>
+								<Input />
+							</div>
+							<div className=''>
+								<h2>Break Duration</h2>
+								<Input />
+							</div>
+						</div>
+					</motion.div>
+				</>
 			)}
 		</AnimatePresence>
 	);
