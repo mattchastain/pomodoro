8755e33da8df19d2cbf2585695a65108c5d07f2b
Matt Chastain <work@mattchastain.dev>
2025-03-31 09:43:03 -0400

    add outline variant, adjust gap

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 	onClick?: (() => void) | undefined;
 	fullWidth?: boolean;
 	disabled?: boolean;
-	variant?: 'default' | 'ghost';
+	variant?: 'default' | 'ghost' | 'outline';
 }
 
 export function Button({
@@ -20,11 +20,12 @@ export function Button({
 	variant = 'default',
 }: ButtonProps) {
 	const baseClasses =
-		'flex items-center h-max p-2 gap-2 rounded-lg text-xs font-bold md:text-sm md:font-medium cursor-pointer ring-offset-background focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
+		'flex items-center h-max p-2 gap-1 rounded-lg font-bold md:text-sm md:font-medium cursor-pointer ring-offset-background focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
 
 	const variantClasses = {
-		default: '',
+		default: 'bg-neutral-300 hover:bg-neutral-200',
 		ghost: 'hover:bg-neutral-300',
+		outline: 'border-2 border-neutral-300 hover:border-neutral-500'
 	};
 
 	return (
