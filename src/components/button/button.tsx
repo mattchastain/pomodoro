452bf79e5046450df7f9a5b89b4a9d716c9c84fe
Matt Chastain <work@mattchastain.dev>
2025-03-31 13:03:25 -0400

    add: classes prop

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,6 +5,7 @@ interface ButtonProps {
 	icon?: LucideIcon;
 	iconSize?: number;
 	onClick?: (() => void) | undefined;
+	classes?: string;
 	fullWidth?: boolean;
 	disabled?: boolean;
 	variant?: 'default' | 'ghost' | 'outline';
@@ -15,12 +16,13 @@ export function Button({
 	icon: Icon,
 	iconSize = 18,
 	onClick,
+	classes,
 	fullWidth = false,
 	disabled = false,
 	variant = 'default',
 }: ButtonProps) {
 	const baseClasses =
-		'flex items-center h-max p-2 gap-1 rounded-lg font-bold md:text-sm md:font-medium cursor-pointer ring-offset-background focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
+		`flex items-center h-max p-2 gap-1 rounded-lg font-bold md:text-sm md:font-medium cursor-pointer ring-offset-background focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 transition-all ${classes}`;
 
 	const variantClasses = {
 		default: 'bg-neutral-300 hover:bg-neutral-200',
