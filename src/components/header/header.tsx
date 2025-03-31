6c7fbbf3b224e7cc9164a0092becd156577aad37
Matt Chastain <work@mattchastain.dev>
2025-03-31 11:02:49 -0400

    enable settings button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,25 @@
 import { AlarmClockCheck, Settings2 } from 'lucide-react';
 import { Button } from '../button/button';
 
-export function Header() {
+interface HeaderProps {
+	setSettingsOpen: (open: boolean) => void;
+}
+
+export function Header({ setSettingsOpen }: HeaderProps) {
 	return (
 		<div className='flex items-center justify-between gap-2'>
 			<div className='flex items-center gap-2'>
 				<AlarmClockCheck />
-				<h1 className='font-bold text-lg md:font-medium md:text-xl'>Pomodoro</h1>
+				<h1 className='font-bold text-lg md:font-medium md:text-xl'>
+					Pomodoro
+				</h1>
 			</div>
-			<Button icon={Settings2} iconSize={24} variant='ghost' />
+			<Button
+				icon={Settings2}
+				iconSize={24}
+				onClick={() => setSettingsOpen(true)}
+				variant='ghost'
+			/>
 		</div>
 	);
 }
