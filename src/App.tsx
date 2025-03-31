debe26b3883b7d19bc2e1fb7ae8ca139d301433d
Matt Chastain <work@mattchastain.dev>
2025-03-31 11:02:08 -0400

    import clock + settings + timercontrols, create resetTimer using useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { Header, Timer } from './components';
+import { useCallback, useState } from 'react';
+import { Clock, Header, Settings, TimerControls } from './components';
 
 export default function App() {
+	const [settingsOpen, setSettingsOpen] = useState(false);
+
+	const [timerActive, setTimerActive] = useState(false);
+	const [timeLeft, setTimeLeft] = useState<number>(60);
+
+	const resetTimer = useCallback(() => {
+		setTimerActive(false);
+		setTimeLeft(60);
+	}, []);
+
 	return (
 		<div className='flex flex-col gap-12 max-w-xl mx-auto'>
-			<Header />
-			<Timer />
+			<Header setSettingsOpen={setSettingsOpen} />
+			<Settings
+				settingsOpen={settingsOpen}
+				setSettingsOpen={setSettingsOpen}
+			/>
+			<Clock
+				timerActive={timerActive}
+				timeLeft={timeLeft}
+				setTimeLeft={setTimeLeft}
+			/>
+			<TimerControls
+				timerActive={timerActive}
+				setTimerActive={setTimerActive}
+				resetTimer={resetTimer}
+			/>
 		</div>
 	);
 }
