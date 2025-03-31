fc5ba57c58adb1922670620e76b5fcafc5521abd
Matt Chastain <work@mattchastain.dev>
2025-03-31 11:03:39 -0400

    add: blur background when open

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,3 +1,22 @@
-export function Settings() {
-	return <div>Settings</div>;
+import { AnimatePresence, motion } from 'framer-motion';
+
+interface SettingsProps {
+	settingsOpen: boolean;
+	setSettingsOpen: (open: boolean) => void;
+}
+
+export function Settings({ settingsOpen, setSettingsOpen }: SettingsProps) {
+	return (
+		<AnimatePresence>
+			{settingsOpen && (
+				<motion.div
+					initial={{ scale: 0.9, opacity: 0 }}
+					animate={{ scale: 1, opacity: 1 }}
+					exit={{ scale: 0.9, opacity: 0 }}
+					onClick={() => setSettingsOpen(false)}
+					className='fixed inset-0 backdrop-blur z-40'
+				></motion.div>
+			)}
+		</AnimatePresence>
+	);
 }
