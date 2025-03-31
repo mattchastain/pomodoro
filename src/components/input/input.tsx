fe7b7321ac5f46cd782bf36aa2411921418f64ee
Matt Chastain <work@mattchastain.dev>
2025-03-31 16:41:31 -0400

    create input and create InputProps interface

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,3 +1,19 @@
-export function Input() {
-	return <div>input</div>;
+interface InputProps {
+	value: number;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function Input({ value, onChange }: InputProps) {
+	return (
+		<input
+			id='study-time'
+			type='number'
+			value={value}
+			min='5'
+			step='5'
+			placeholder={value.toString()}
+			onChange={onChange}
+			className='border-2 border-neutral-500/50 rounded-lg p-2 w-full'
+		></input>
+	);
 }
