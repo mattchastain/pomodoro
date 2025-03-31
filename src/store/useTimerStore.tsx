ea3a3f31146b1d787e9bc959db74b39dbc32d615
Matt Chastain <work@mattchastain.dev>
2025-03-31 12:53:30 -0400

    create TimerState and useTimerStore

--- src/store/useTimerStore.tsx
+++ src/store/useTimerStore.tsx
@@ -1,3 +1,17 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+type TimerState = {
+	studyDuration: number;
+	breakDuration: number;
+};
+
+export const useTimerStore = create<TimerState>()(
+	persist(
+		(set) => ({
+			studyDuration: 60 * 45,
+			breakDuration: 60 * 10,
+		}),
+		{ name: 'timer-store' }
+	)
+);
