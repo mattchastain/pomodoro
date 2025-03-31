5d19abb588575abebf03b699f48973991042807f
Matt Chastain <work@mattchastain.dev>
2025-03-31 08:58:15 -0400

    set defaults for props, create baseClasses/variantClasses, apply to button and return

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -11,11 +11,30 @@ interface ButtonProps {
 
 export function Button({
 	label,
-	icon,
+	icon: Icon,
 	onClick,
-	fullWidth,
-	disabled,
-	variant,
+	fullWidth = false,
+	disabled = false,
+	variant = 'default',
 }: ButtonProps) {
-	return <button></button>;
+	const baseClasses =
+		'flex items-center h-max p-2 gap-2 rounded-lg text-xs font-bold md:text-sm md:font-medium cursor-pointer ring-offset-background focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
+
+	const variantClasses = {
+		default: '',
+		ghost: '',
+	};
+
+	return (
+		<button
+			onClick={onClick}
+			disabled={disabled}
+			className={`${baseClasses} ${fullWidth && 'w-full'} ${
+				variantClasses[variant]
+			}`}
+		>
+			{Icon && <Icon size={18} />}
+			{label}
+		</button>
+	);
 }
