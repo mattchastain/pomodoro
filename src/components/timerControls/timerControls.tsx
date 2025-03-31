a5c3ffba99b692bd1eff787b09cb71add8a2c454
Matt Chastain <work@mattchastain.dev>
2025-03-31 11:05:11 -0400

    create Props interface, create toggleTimerActive with useCallback, create two buttons for start/stop + reset

--- src/components/timerControls/timerControls.tsx
+++ src/components/timerControls/timerControls.tsx
@@ -1,3 +1,36 @@
-export function TimerControls() {
-	return <div>timerControls</div>;
+import { Pause, Play, RotateCcw } from 'lucide-react';
+import { useCallback } from 'react';
+import { Button } from '../button/button';
+
+interface TimerControlsProps {
+	timerActive: boolean;
+	setTimerActive: (active: boolean) => void;
+	resetTimer: () => void;
+}
+
+export function TimerControls({
+	timerActive,
+	setTimerActive,
+	resetTimer,
+}: TimerControlsProps) {
+	const toggleTimerActive = useCallback(() => {
+		setTimerActive(!timerActive);
+	}, [timerActive]);
+
+	return (
+		<div className='flex items-center justify-center gap-2'>
+			<Button
+				label={timerActive ? 'Pause' : 'Start'}
+				icon={timerActive ? Pause : Play}
+				onClick={toggleTimerActive}
+				variant='outline'
+			/>
+			<Button
+				icon={RotateCcw}
+				label='Reset'
+				onClick={resetTimer}
+				variant='outline'
+			/>
+		</div>
+	);
 }
