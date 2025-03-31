6e446300dec7e553658bfec2a80503c76bef9a1b
Matt Chastain <work@mattchastain.dev>
2025-03-31 09:08:35 -0400

    import icons, create Pomodoro h1, import Button for settings

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,14 @@
+import { AlarmClockCheck, Settings2 } from 'lucide-react';
+import { Button } from '../button/button';
+
 export function Header() {
-  return (
-    <div>header</div>
-  )
+	return (
+		<div className='flex items-center justify-between gap-2'>
+			<div className='flex items-center gap-2'>
+				<AlarmClockCheck />
+				<h1 className='font-medium md:text-xl'>Pomodoro</h1>
+			</div>
+			<Button icon={Settings2} iconSize={24} variant='ghost' />
+		</div>
+	);
 }
