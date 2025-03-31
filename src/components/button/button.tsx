02e7a85f645e7631aef21463fc6357c7020b096d
Matt Chastain <work@mattchastain.dev>
2025-03-31 08:49:11 -0400

    create ButtonProps interface

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,21 @@
-export function Button() {
-	return <div>button</div>;
+import { LucideIcon } from 'lucide-react';
+
+interface ButtonProps {
+	label?: string;
+	icon?: LucideIcon;
+	onClick?: (() => void) | undefined;
+	fullWidth?: boolean;
+	disabled?: boolean;
+	variant?: 'default' | 'ghost';
+}
+
+export function Button({
+	label,
+	icon,
+	onClick,
+	fullWidth,
+	disabled,
+	variant,
+}: ButtonProps) {
+	return <button></button>;
 }
