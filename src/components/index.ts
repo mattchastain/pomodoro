04d05c582f65ba76a65323819d3d516d2642ffe1
Matt Chastain <work@mattchastain.dev>
2025-03-31 10:50:55 -0400

    export settings

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button/button';
 export * from './clock/clock';
 export * from './header/header';
+export * from './settings/settings';
 export * from './timerControls/timerControls';
