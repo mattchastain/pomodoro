77bdac92b92a14b65e2e5d91e508e34d8b192659
Matt Chastain <work@mattchastain.dev>
2025-03-31 13:06:08 -0400

    import useTimerStore, pass isBreak to clock, pass settingsOpen to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { useCallback, useState } from 'react';
 import { Clock, Header, Settings, TimerControls } from './components';
+import { useTimerStore } from './store/useTimerStore';
 
 export default function App() {
+	const { studyDuration, breakDuration } = useTimerStore();
+	const [isBreak, setIsBreak] = useState(false);
+	const initialTime = isBreak ? breakDuration : studyDuration;
+
 	const [settingsOpen, setSettingsOpen] = useState(false);
 
 	const [timerActive, setTimerActive] = useState(false);
-	const [timeLeft, setTimeLeft] = useState<number>(60);
+	const [timeLeft, setTimeLeft] = useState<number>(initialTime);
 
 	const resetTimer = useCallback(() => {
 		setTimerActive(false);
-		setTimeLeft(60);
+		setTimeLeft(initialTime);
 	}, []);
 
 	return (
 		<div className='flex flex-col gap-12 max-w-xl mx-auto'>
-			<Header setSettingsOpen={setSettingsOpen} />
+			<Header
+				settingsOpen={settingsOpen}
+				setSettingsOpen={setSettingsOpen}
+			/>
 			<Settings
 				settingsOpen={settingsOpen}
 				setSettingsOpen={setSettingsOpen}
@@ -23,6 +31,7 @@ export default function App() {
 				timerActive={timerActive}
 				timeLeft={timeLeft}
 				setTimeLeft={setTimeLeft}
+				isBreak={isBreak}
 			/>
 			<TimerControls
 				timerActive={timerActive}
